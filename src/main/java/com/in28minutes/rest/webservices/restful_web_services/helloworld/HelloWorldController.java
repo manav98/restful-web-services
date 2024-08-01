cbaaa27232b1698c633a06c1ef0c3332d55a8cf5
Manav-NTT <manav12.gupta@nttdata.com>
2024-08-01 13:08:09 +0530

    hello world

---
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,7 @@
+package com.in28minutes.rest.webservices.restful_web_services.helloworld;
+
+public class HelloWorldController {
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
